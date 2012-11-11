f4245f221133714f53f55f4122a193a6b119c54f
Le Phat Huy <huy1208@gmail.com>
2012-11-11 17:26:53 +0900

    first commit

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://myphamhan.tk/mp/admin/');
+define('HTTP_CATALOG', 'http://myphamhan.tk/mp/');
+define('HTTP_IMAGE', 'http://myphamhan.tk/mp/image/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://myphamhan.tk/mp/admin/');
+define('HTTPS_IMAGE', 'http://myphamhan.tk/mp/image/');
+
+// DIR
+define('DIR_APPLICATION', 'E:\work\wamp\www\mp/admin/');
+define('DIR_SYSTEM', 'E:\work\wamp\www\mp/system/');
+define('DIR_DATABASE', 'E:\work\wamp\www\mp/system/database/');
+define('DIR_LANGUAGE', 'E:\work\wamp\www\mp/admin/language/');
+define('DIR_TEMPLATE', 'E:\work\wamp\www\mp/admin/view/template/');
+define('DIR_CONFIG', 'E:\work\wamp\www\mp/system/config/');
+define('DIR_IMAGE', 'E:\work\wamp\www\mp/image/');
+define('DIR_CACHE', 'E:\work\wamp\www\mp/system/cache/');
+define('DIR_DOWNLOAD', 'E:\work\wamp\www\mp/download/');
+define('DIR_LOGS', 'E:\work\wamp\www\mp/system/logs/');
+define('DIR_CATALOG', 'E:\work\wamp\www\mp/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'huy');
+define('DB_PASSWORD', 'anhhuy');
+define('DB_DATABASE', 'vnshopping');
+define('DB_PREFIX', 'opencart_');
+?>
---
+++ config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://myphamhan.tk/mp/');
+define('HTTP_IMAGE', 'http://myphamhan.tk/mp/image/');
+define('HTTP_ADMIN', 'http://myphamhan.tk/mp/admin/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://myphamhan.tk/mp/');
+define('HTTPS_IMAGE', 'http://myphamhan.tk/mp/image/');
+
+// DIR
+define('DIR_APPLICATION', 'E:\work\wamp\www\mp/catalog/');
+define('DIR_SYSTEM', 'E:\work\wamp\www\mp/system/');
+define('DIR_DATABASE', 'E:\work\wamp\www\mp/system/database/');
+define('DIR_LANGUAGE', 'E:\work\wamp\www\mp/catalog/language/');
+define('DIR_TEMPLATE', 'E:\work\wamp\www\mp/catalog/view/theme/');
+define('DIR_CONFIG', 'E:\work\wamp\www\mp/system/config/');
+define('DIR_IMAGE', 'E:\work\wamp\www\mp/image/');
+define('DIR_CACHE', 'E:\work\wamp\www\mp/system/cache/');
+define('DIR_DOWNLOAD', 'E:\work\wamp\www\mp/download/');
+define('DIR_LOGS', 'E:\work\wamp\www\mp/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'huy');
+define('DB_PASSWORD', 'anhhuy');
+define('DB_DATABASE', 'vnshopping');
+define('DB_PREFIX', 'opencart_');
+?>
